d55db2c3646f72d0a6deb1678e450c86b322046b
Kane <kane.web.marketing@gmail.com>
2017-07-13 16:44:13 -0700

    Combining built-in indicators with TA-lib indicators

---
+++ Kane2.js
@@ -0,0 +1,115 @@
+/*
+  
+  RSI - cykedev 14/02/2014
+
+  (updated a couple of times since, check git history)
+
+ */
+// helpers
+var _ = require('lodash');
+var log = require('../core/log.js');
+
+// get config
+var config = require('../core/util.js').getConfig();
+var settings = config.Kane2;
+//var ADXValue = settings.ADX.value;
+
+var RSI = require('./indicators/RSI.js');
+
+// let's create our own method
+var method = {};
+
+// prepare everything our method needs
+method.init = function() {
+  this.name = 'Kane2';
+
+  this.trend = {
+    direction: 'none',
+    duration: 0,
+    persisted: false,
+    adviced: false
+  };
+
+  this.requiredHistory = config.tradingAdvisor.historySize;
+
+  // define the indicators we need
+  this.addIndicator('rsi', 'RSI', { interval: settings.RSI.interval });  
+  //this.addTalibIndicator('myadx', 'adx', { optInTimePeriod: settings.ADX.period });
+}
+
+// for debugging purposes log the last 
+// calculated parameters.
+method.log = function(candle) {
+  var digits = 8;
+  var rsi = this.indicators.rsi;
+
+  log.debug('calculated RSI properties for candle:');
+  log.debug('\t', 'rsi:', rsi.rsi.toFixed(digits));
+  log.debug('\t', 'price:', candle.close.toFixed(digits));
+}
+
+method.check = function() {
+  // check ADX
+  //var ADXResult = this.talibIndicators.myadx.result['outReal'];
+  //var ADXTrend = ADXResult > ADXValue;   
+  
+  var rsi = this.indicators.rsi;
+  var rsiVal = rsi.rsi;
+
+  if(rsiVal > settings.RSI.high) {
+
+    // new trend detected
+    if(this.trend.direction !== 'high')
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'high',
+        adviced: false
+      };
+
+    this.trend.duration++;
+
+    log.debug('In high since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= settings.RSI.persistence)
+      this.trend.persisted = true;
+
+    if(this.trend.persisted && !this.trend.adviced) {
+      this.trend.adviced = true;
+      this.advice('short');
+    } else
+      this.advice();
+    
+  } else if(rsiVal < settings.RSI.low) {
+
+    // new trend detected
+    if(this.trend.direction !== 'low')
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'low',
+        adviced: false
+      };
+
+    this.trend.duration++;
+
+    log.debug('In low since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= settings.RSI.persistence)
+      this.trend.persisted = true;
+
+    if(this.trend.persisted && !this.trend.adviced) {
+      this.trend.adviced = true;
+      this.advice('long');
+    } else
+      this.advice();
+
+  } else {
+
+    log.debug('In no trend');
+
+    this.advice();
+  }
+}
+
+module.exports = method;
